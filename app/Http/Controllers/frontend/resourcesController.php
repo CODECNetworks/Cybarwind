c802bdc93537cb5cce21e28c93ba576619a91edc
vijaycodec <vijaycodec@gmail.com>
2025-01-15 17:31:30 +0530

    fix resouce frontent page

--- app/Http/Controllers/frontend/resourcesController.php
+++ app/Http/Controllers/frontend/resourcesController.php
@@ -4,14 +4,23 @@ namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Resource;
 use Illuminate\Http\Request;
 
 class resourcesController extends Controller
 {
     public function index()
     {
-        $categories = Category::pluck('name');
+          $categories1 = Category::pluck('name'); 
         
-        return view('frontend.resources',compact('categories'));
+        // $categories = Category::with('resources')->get();
+
+        $categories = Category::with(['resources' => function($query) {
+            // Get only the first resource for each category
+            $query->distinct()->take(1);
+        }])->get();
+        
+    
+        return view('frontend.resources',compact('categories','categories1'));
     }
 }
